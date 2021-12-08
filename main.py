f8eea5dca3163bd42654931ffa6897d24b1d88d8
Diegogdz <diegodoy@alumnos.uai.cl>
2021-12-07 23:38:38 -0300

    13commit

--- main.py
+++ main.py
@@ -45,26 +45,9 @@ def equipo():
     return render_template("/equipo.html")        
 
 
-@app.route("/contacto" , methods=["POST"])
-def conact():
-    request.form["Nombre"]
-    Nombre = request.form["Nombre"]
-    Empresa = request.form["Empresa"]
-    Mail = request.form["Mail"]
-    return redirect('/')
+
    
 
-@app.route('/users', methods=['POST'])
-def create_user():
-    print("Got Post Info")
-    print(request.form)
-    name_from_form = request.form['name']
-    email_from_form = request.form['email']
-    return render_template(
-        "show.html",
-        name_on_template=name_from_form,
-        email_on_template=email_from_form
-    )
 
 
 
